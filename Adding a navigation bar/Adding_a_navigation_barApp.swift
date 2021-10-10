c3204eee570f2a8dd96f357b90cebc6d18e3aab6
Franco Rod <81878824+Francorod@users.noreply.github.com>
2021-10-09 20:43:08 -0400

    Initial Commit

---
+++ Adding a navigation bar/Adding_a_navigation_barApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adding_a_navigation_barApp.swift
+//  Adding a navigation bar
+//
+//  Created by Franco Rodrigues on 10/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Adding_a_navigation_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
